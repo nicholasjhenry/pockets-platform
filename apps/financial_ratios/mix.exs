3b9ca6e29e53d376c9800328af5d7dff682ed5e2
Nicholas Henry <nicholas.henry@gmail.com>
2021-08-17 12:38:10 -0400

    Configure docs with Math typesetting library

--- apps/financial_ratios/mix.exs
+++ apps/financial_ratios/mix.exs
@@ -4,6 +4,7 @@ defmodule FinancialRatios.MixProject do
   def project do
     [
       app: :financial_ratios,
+      name: "Financial Ratios",
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -11,7 +12,8 @@ defmodule FinancialRatios.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -27,5 +29,27 @@ defmodule FinancialRatios.MixProject do
     [
       {:sidecar, in_umbrella: true}
     ]
+  end
+
+  defp docs do
+    [
+      before_closing_head_tag: &before_closing_head_tag/1
+    ]
+  end
+
+  defp before_closing_head_tag(_format), do: include_math_typsetting_lib()
+
+  # [Katex](https://katex.org)
+  defp include_math_typsetting_lib do
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css" integrity="sha384-RZU/ijkSsFbcmivfdRBQDtwuwVqK7GMOw6IMvKyeWL2K5UAlyp6WonmB8m7Jd0Hn" crossorigin="anonymous">
+
+    <!-- The loading of KaTeX is deferred to speed up page rendering -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.js" integrity="sha384-pK1WpvzWVBQiP0/GjnvRxV4mOb0oxFuyRxJlk6vVw146n3egcN5C925NCP7a7BY8" crossorigin="anonymous"></script>
+
+    <!-- To automatically render math in text elements, include the auto-render extension: -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/contrib/auto-render.min.js" integrity="sha384-vZTG03m+2yp6N6BNi5iM4rW4oIwk5DfcNdFfxkk9ZWpDriOkXX8voJBFrAO7MpVl" crossorigin="anonymous"
+        onload="renderMathInElement(document.body);"></script>
+    """
   end
 end
