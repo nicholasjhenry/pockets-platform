867f83999fccb1be091d8fccdc376bdcbd2fc67e
Nicholas Henry <nicholas.henry@gmail.com>
2021-08-17 12:38:22 -0400

    Add docs for financial ratios

--- apps/financial_ratios/lib/financial_ratios.ex
+++ apps/financial_ratios/lib/financial_ratios.ex
@@ -1,2 +1,7 @@
 defmodule FinancialRatios do
+  @moduledoc """
+  > Financial ratios are powerful tools to help summarize financial statements and the health of a company or enterprise.
+  >
+  > --- [Investopedia](https://www.investopedia.com/financial-ratios-4689817)
+  """
 end
